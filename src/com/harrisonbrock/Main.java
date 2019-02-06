0ce85c28400c268f5cac0ee8fe523c40f7c335dd
Harrison Brock <harrison@harrisonbrock.com>
2019-02-06 17:37:02 -0500

    create more tests

--- src/com/harrisonbrock/Main.java
+++ src/com/harrisonbrock/Main.java
@@ -10,11 +10,20 @@ public class Main {
         List<AnimalImpl> list = new LinkedList<>();
         list.add(new Mammal("Panda", 1869));
         list.add(new Mammal("Zebra", 1778));
+        list.add(new Mammal("Koala", 1816));
+        list.add(new Mammal("Sloth", 1804));
+        list.add(new Mammal("Raccoon", 1758));
+        list.add(new Mammal("Bigfoot", 2021));
+
         list.add(new Bird("Parrot", 1884));
+        list.add(new Bird("Pigeon", 1837));
+        list.add(new Bird("Peacock", 1821));
+        list.add(new Bird("Toucan", 1758));
+        list.add(new Bird("Swan", 1758));
 
         for (AnimalImpl animal : list) {
             animal.breath();
-            System.out.println("id: " +  animal.getId() + " | " + animal.getName() + " | " + animal.getYearDiscovered());
+            System.out.println("id: " +  animal.getId() + " | " + animal.getName() + " | " + animal.getYearDiscovered() + " | " + animal.breath());
 
         }
 
