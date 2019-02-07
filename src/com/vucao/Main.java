6d12c736a923f984b4fbb9179de3c0d9bd0031e3
Vu Cao <cdhvu@yahoo.com>
2019-02-06 16:32:09 -0800

    Completed MVP

--- src/com/vucao/Main.java
+++ src/com/vucao/Main.java
@@ -1,6 +1,8 @@
 package com.vucao;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
 
 public class Main
 {
@@ -31,6 +33,44 @@ public class Main
             System.out.println(a.getId() + " " + a.getName() + " " + a.getYear() + " " + a.move() + " " + a.breath() + " " + a.reproduce());
         }
 
+        System.out.println("*** All the animals in descending order by year named ***");
+        animalArrayList.sort((a1, a2) -> a2.getYear() - a1.getYear());
+        animalArrayList.forEach(a -> System.out.println(a.getName() + " " + a.getYear()));
+        System.out.println("\n");
 
+        System.out.println("*** All the animals alphabetically ***");
+        animalArrayList.sort((a1, a2) -> a1.getName().compareToIgnoreCase(a2.getName()));
+        animalArrayList.forEach(a -> System.out.println(a.getName()));
+        System.out.println("\n");
+
+        System.out.println("*** All the animals ordered by how they move ***");
+        animalArrayList.sort((a1, a2) -> a1.move().compareToIgnoreCase(a2.move()));
+        animalArrayList.forEach(a -> System.out.println(a.getName() + " " + a.move()));
+        System.out.println("\n");
+
+        System.out.println("*** All animals that breath with lungs ***");
+        ArrayList<AbstractAnimal> filteredAnimals = new ArrayList<>(animalArrayList);
+        filteredAnimals.removeIf(a -> !a.breath().equals("lungs"));
+        filteredAnimals.forEach(a -> System.out.println(a.getName() + " breaths with " + a.breath()));
+        System.out.println("\n");
+
+        System.out.println("*** All animals that breath with lungs and were named in 1758 ***");
+        ArrayList<AbstractAnimal> filteredAnimals2 = new ArrayList<>(animalArrayList);
+        filteredAnimals2.removeIf(a -> !(a.breath().equals("lungs") && a.getYear()==1758));
+        filteredAnimals2.forEach(a -> System.out.println(a.getName() + " breaths with " + a.breath() + " and was named in " + a.getYear()));
+        System.out.println("\n");
+
+        System.out.println("*** All animals that lay eggs and breath with lungs ***");
+        ArrayList<AbstractAnimal> filteredAnimals3 = new ArrayList<>(animalArrayList);
+        filteredAnimals3.removeIf(a -> !(a.breath().equals("lungs") && a.reproduce().equals("eggs")));
+        filteredAnimals3.forEach(a -> System.out.println(a.getName() + " breaths with " + a.breath() + " lays " + a.reproduce()));
+        System.out.println("\n");
+
+        System.out.println("*** All animals that were named in 1758 order by name ***");
+        ArrayList<AbstractAnimal> filteredAnimals4 = new ArrayList<>(animalArrayList);
+        filteredAnimals4.removeIf(a -> a.getYear()!=1758);
+        filteredAnimals4.sort((a1, a2) -> a1.getName().compareToIgnoreCase(a2.getName()));
+        filteredAnimals4.forEach(a -> System.out.println(a.getName() + " was named in " + a.getYear()));
+        System.out.println("\n");
     }
 }
