2372128410e1eeb3fe544e4084223865ddf7eef2
Maryna Longnickel <37321032+MarynaLongnickel@users.noreply.github.com>
2019-02-06 21:16:09 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ package AnimalKingdom;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -30,15 +32,33 @@ public class Main {
         myList.add(new Fish("Catfish",1817));
         myList.add(new Fish("Perch",1758));
 
-        System.out.println("__________________ List by year __________________");
+        System.out.println("__________________ Animals listed by year __________________");
         orderByYear(myList);
 
-        System.out.println("__________________ List alphabetically __________________");
+        System.out.println("__________________ Animals listed alphabetically __________________");
         myList.sort((a1, a2) -> a1.getName().compareToIgnoreCase(a2.getName()));
         for (Animal ani : myList) System.out.println(ani.getName());
 
+        System.out.println("__________________ Animals listed by locomotion __________________");
         myList.sort((a1, a2) -> a1.move().compareToIgnoreCase(a2.move()));
-        for (Animal ani : myList) System.out.println(ani.getName() + ":" + ani.move());
+        for (Animal ani : myList) System.out.println(ani.getName() + " : " + ani.move());
+
+        System.out.println("__________________ Animals the breathe with lungs __________________");
+        List<Animal> filtered = myList.stream().filter(a -> a.breathe() == "lungs").collect(Collectors.toList());
+        filtered.forEach(ani -> System.out.println(ani.getName()));
+
+        System.out.println("__________________ Animals the breathe with lungs and were named in 1758 __________________");
+        List<Animal> filtered2 = myList.stream().filter(a -> a.breathe() == "lungs" && a.getYear() == 1758).collect(Collectors.toList());
+        filtered2.forEach(ani -> System.out.println(ani.getName()));
+
+        System.out.println("__________________ Animals the breathe with lungs and lay eggs __________________");
+        List<Animal> filtered3 = myList.stream().filter(a -> a.breathe() == "lungs" && a.reproduce() == "eggs").collect(Collectors.toList());
+        filtered3.forEach(ani -> System.out.println(ani.getName()));
+
+        System.out.println("__________________ Animals that were named in 1758 listed alphabetically __________________");
+        List<Animal> filtered4 = myList.stream().filter(a -> a.getYear() == 1758).collect(Collectors.toList());
+        filtered4.sort((a1, a2) -> a1.getName().compareToIgnoreCase(a2.getName()));
+        filtered4.forEach(ani -> System.out.println(ani.getName() + " : " + ani.getYear()));
 
     }
 }
