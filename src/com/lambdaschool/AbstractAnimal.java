2208ddb511292c0171c2d4b2c80c84396bdd0d8a
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-02-06 18:39:14 -0800

    Added methods for setName & setYearNamed

--- src/com/lambdaschool/AbstractAnimal.java
+++ src/com/lambdaschool/AbstractAnimal.java
@@ -22,9 +22,19 @@ public abstract class AbstractAnimal
         return name;
     }
 
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
     public int getYearNamed()
     {
         return yearNamed;
+    }
+
+    public void setYearNamed(int yearNamed)
+    {
+        this.yearNamed = yearNamed;
     }
 
     public int getId()
