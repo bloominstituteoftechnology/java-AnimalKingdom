82917e8a251c4d7c60d0f9599ef11ebb3bff3429
nchen0 <nicky.chen0@gmail.com>
2019-02-10 12:30:44 -0500

    Mammal, Bird, Fish classes

---
+++ src/com/lambda/Fish.java
@@ -0,0 +1,22 @@
+package com.lambda;
+
+public class Fish extends Animal {
+    public Fish(String name, int yearDiscovered) {
+        super(name, yearDiscovered);
+    }
+
+    @Override
+    public String move() {
+        return "Swims";
+    }
+
+    @Override
+    public String breathe() {
+        return "Gills";
+    }
+
+    @Override
+    public String reproduce() {
+        return "Eggs";
+    }
+}
