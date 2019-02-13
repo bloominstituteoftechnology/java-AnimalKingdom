2ac1b27f81d31bba5896a8a761cd914ef936fdb2
Axhon Ruiz <axhonruiz@gmail.com>
2019-02-12 23:21:58 -0500

    finished assignment

--- src/com/lambdaschool/Main.java
+++ src/com/lambdaschool/Main.java
@@ -38,7 +38,37 @@ public class Main {
         myAnimals.forEach(animal -> System.out.println(animal.getName()));
 
         System.out.println("\n*** List animals by how they move ***");
-        myAnimals.sort((a1,a2) -> a1.move().compareToIgnoreCase(a2.move()));
+        myAnimals.sort((a1, a2) -> a1.move().compareToIgnoreCase(a2.move()));
         myAnimals.forEach(animal -> System.out.println(animal.getName() + " " + animal.move()));
+
+        System.out.println("\n*** List only animals that breathe with lungs ***");
+        myAnimals.forEach(animal -> {
+            if (animal.breathe().equalsIgnoreCase("lungs")) {
+                System.out.println(animal.getName());
+            }
+        });
+
+        System.out.println("\n*** List only those animals that breath with lungs and were named in 1758 ***");
+        myAnimals.forEach(animal -> {
+            if (animal.breathe().equalsIgnoreCase("lungs") && animal.getYearDiscovered() == 1758) {
+                System.out.println(animal.getName());
+            }
+        });
+
+        System.out.println("\n*** List only those animals that lay eggs and breath with lungs ***");
+        myAnimals.forEach(animal -> {
+            if (animal.reproduce().equalsIgnoreCase("eggs") && animal.breathe().equalsIgnoreCase("lungs")) {
+                System.out.println(animal.getName());
+            }
+        });
+
+        System.out.println("\n*** List alphabetically only those animals that were named in 1758 ***");
+        // because the list is small we can sort the whole thing. this is probably not what the assignment hoped for.
+        myAnimals.sort((a1, a2) -> a1.getName().compareToIgnoreCase(a2.getName()));
+        myAnimals.forEach(animal -> {
+            if(animal.getYearDiscovered() == 1758){
+                System.out.println(animal.getName());
+            }
+        });
     }
 }
