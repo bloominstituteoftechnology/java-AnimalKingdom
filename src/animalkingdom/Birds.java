ac4f2a9a6617e229990eb51cd490034666d75383
dangolbeeker <scottb305@outlook.com>
2019-09-04 18:08:58 -0400

    Birds and fish classes finished

--- src/animalkingdom/Birds.java
+++ src/animalkingdom/Birds.java
@@ -1,1 +1,22 @@
 package animalkingdom;
+
+public class Birds extends AbstractAnimal {
+    public Birds(String name, int year) {
+        super(name, year);
+    }
+    
+    @Override
+    public String move() {
+        return does("flies");
+  }
+
+  @Override
+  public String breath() {
+    return does("breathes with its lungs");
+  }
+
+  @Override
+  public String reproduce() {
+    return does("produces eggs");
+  }
+}
--- src/animalkingdom/Fish.java
+++ src/animalkingdom/Fish.java
@@ -1,1 +1,24 @@
 package animalkingdom;
+
+
+public class Fish extends AbstractAnimal {
+    public Fish(String name, int year) {
+        super(name, year);
+    }
+
+    @Override
+    public String move() {
+        return does("swims");
+    }
+
+    
+  @Override
+  public String breath() {
+    return does("breathes with its gills");
+  }
+
+  @Override
+  public String reproduce() {
+    return does("produces eggs");
+  }
+}
