b3b5b1600adfa3818e2fe5612537904ca4767eef
dangolbeeker <scottb305@outlook.com>
2019-09-04 18:02:14 -0400

    Mammals class added

--- src/animalkingdom/Mammals.java
+++ src/animalkingdom/Mammals.java
@@ -1,1 +1,22 @@
 package animalkingdom;
+
+public class Mammals extends AbstractAnimal {
+    public Mammals(Strig name, int year) {
+        super(name, year);
+    }
+
+    @Override
+    public String move() {
+        return does("walks");
+    }
+  
+    @Override
+    public String breath() {
+      return does("breathes with its lungs");
+    }
+  
+    @Override
+    public String reproduce() {
+      return does("births its child(ren)");
+    }
+  }
