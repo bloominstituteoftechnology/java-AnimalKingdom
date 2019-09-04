b6b4c3f971cab1d1624eaacabbf3c75dd05fd382
dangolbeeker <scottb305@outlook.com>
2019-09-04 17:44:34 -0400

    Animal Abstract Class added

--- src/animalkingdom/AbstractAnimal.java
+++ src/animalkingdom/AbstractAnimal.java
@@ -1,2 +1,22 @@
 package animalkingdom;
 
+public abstract class AbstractAnimal {
+    private static int currentId = 0;
+    private int id;
+    private String name;
+    private int yearDiscovered;
+
+    public AbstractAnimal(String name, int year) {
+        id = currentId++;
+        this.name = name;
+        yearDiscovered = year;
+    }
+
+    public abstract String move();
+    public abstract String breath();
+    public abstract String reproduce();
+
+    public String eatsFood(String food) {
+        return name + " eats " + food + ".";
+    }
+}
--- src/animalkingdom/Main.java
+++ src/animalkingdom/Main.java
@@ -1,1 +1,7 @@
 package animalkingdom;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
