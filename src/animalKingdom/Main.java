c3a1ab7bd72b5549beaef66250a66eebdfa48182
Noah Franco <noah@macbooks-mbp.lan>
2020-10-25 14:04:03 -0400

    initialzed app

---
+++ src/animalKingdom/Main.java
@@ -0,0 +1,12 @@
+package animalKingdom;
+
+public class Main {
+
+    private static void initialData() {
+        System.out.println("Initializing App");
+    }
+
+    public static void main(String[] args) {
+        initialData();
+    }
+}
