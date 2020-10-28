8e7a7ba533a2fa69cb29e31fa3915c2dd7818bf4
aldairbalanzar <abalanzar7@gmail.com>
2020-10-27 21:34:10 -0400

    'test'

--- animalKingdomPackage/Main.java
+++ animalKingdomPackage/Main.java
@@ -2,6 +2,7 @@ package animalKingdomPackage;
 
 public class Main {
     private static void animalKingdom() {
+        // Mammals
         Mammal panda = new Mammal("Panda", 1869);
         Mammal zebra = new Mammal("Zebra", 1778);
         Mammal koala = new Mammal("Koala", 1816);
@@ -10,12 +11,14 @@ public class Main {
         Mammal sloth = new Mammal("Sloth", 1758);
         Mammal bigfoot = new Mammal("Bigfoot", 2021);
 
+        // Birds
         Bird pigeon = new Bird("Pigeon", 1837);
         Bird peacock = new Bird("Peacock", 1821);
         Bird toucan = new Bird("Toucan", 1758);
         Bird parrot = new Bird("Parrot", 1824);
         Bird swan = new Bird("Swan", 1758);
 
+        // Fish
         Fish salmon = new Fish("Salmon", 1758);
         Fish catfish = new Fish("Catfish", 1817);
         Fish perch = new Fish("Perch", 1758);
